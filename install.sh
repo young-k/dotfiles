0853ae04a1ea6f43d97d97cacf4a325d55d506ca
Young Kim <young-k@protonmail.com>
2017-07-27 16:20:51 -0400

    added vundle installation

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ cd ~
 cp -f ~/dotfiles/.bashrc ~/.bashrc
 cp -Rf ~/dotfiles/dotvim/.vim ~/.vim
 cp -f ~/dotfiles/dotvim/.vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+source ~/.bashrc
