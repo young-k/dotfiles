79dd7cd51257ef30795f02e005ce4a988309b06c
Young Kim <young-k@protonmail.com>
2018-05-29 20:07:41 -0700

    commiting file instead of symlink

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 rsync -a ~/.gitignore_global ~/dotfiles/.gitignore_global
 rsync -a ~/.vimrc ~/dotfiles/dotvim/.vimrc
-rsync -a ~/.zshrc ~/dotfiles/dotprezto/.zshrc
-rsync -a ~/.zpreztorc ~/dotfiles/dotprezto/.zpreztorc
+rsync -a ~/.zprezto/runcoms/zshrc ~/dotfiles/dotprezto/.zshrc
+rsync -a ~/.zprezto/runcoms/zpreztorc ~/dotfiles/dotprezto/.zpreztorc
 rsync -a ~/.zprezto/modules/prompt/functions/prompt_sorin_setup ~/dotfiles/dotprezto/prompt_sorin_setup
