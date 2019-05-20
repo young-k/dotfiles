1426430956a3294609605e55001f7bf2285fdf68
Young Kim <yk465@cornell.edu>
2019-05-20 12:56:27 -0700

    Update installation scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 cd ~
+
+echo 'Copying bash configs...'
 cp -f ~/dotfiles/.bashrc ~/.bashrc
+
+echo 'Copying vim configs...'
 cp -Rf ~/dotfiles/dotvim/.vim ~/.vim
 cp -f ~/dotfiles/dotvim/.vimrc ~/.vimrc
+
+echo 'Copying global gitignore...'
 cp -f ~/dotfiles/.gitignore_global ~/.gitignore_global
+
+echo 'Copying zsh configs...'
+cp -f ~/dotfiles/.zpreztorc ~/.zpreztorc
+cp -f ~/dotfiles/.zshrc ~/.zshrc
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 source ~/.bashrc
