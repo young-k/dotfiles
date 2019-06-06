032a09d01de59951daf3f9643a98d9f64a81aec7
Young Kim <youngkim@fb.com>
2019-06-06 11:31:22 +0100

    Update with tmux.conf

--- sync.sh
+++ sync.sh
@@ -1,5 +1,9 @@
 rsync -a ~/.gitignore_global ~/dotfiles/.gitignore_global
-rsync -a ~/.vimrc ~/dotfiles/dotvim/.vimrc
-rsync -a ~/.zshrc ~/dotfiles/dotprezto/.zshrc
-rsync -a ~/.zpreztorc ~/dotfiles/dotprezto/.zpreztorc
+
+rsync -a ~/.vimrc ~/dotfiles/dotvim/vimrc
+
+rsync -a ~/.tmux.conf ~/dotfiles/dotprezto/tmux.conf
+
+rsync -a ~/.zshrc ~/dotfiles/dotprezto/zshrc
+rsync -a ~/.zpreztorc ~/dotfiles/dotprezto/zpreztorc
 rsync -a ~/.zprezto/modules/prompt/functions/prompt_sorin_setup ~/dotfiles/dotprezto/prompt_sorin_setup
